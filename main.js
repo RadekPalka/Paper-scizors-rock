d17a562811d7ff8665c7d558dfcd79ee2919657c
Radoslaw Palka <radekpalka@gmail..com>
2023-02-11 16:51:59 +0100

    add  result condition and display result and update summary

--- main.js
+++ main.js
@@ -1,10 +1,72 @@
 const images = document.querySelectorAll(".container>img")
+const computerChoiceImage = document.querySelector(".computer-choice>img")
+const resultParagraph = document.querySelector(".result>p")
+const winsSpan = document.querySelector(".wins>span")
+const losesSpan = document.querySelector(".loses>span")
+const drawsSpan = document.querySelector(".draws>span")
+
+const fightWithRock = computerChoice =>{
+  if (computerChoice === "rock"){
+    resultParagraph.textContent = "It's draw"
+    drawsSpan.textContent= +drawsSpan.textContent +1
+  }
+  else if (computerChoice === "paper"){
+    resultParagraph.textContent = "You lose"
+    losesSpan.textContent= +losesSpan.textContent +1
+  }
+  else if (computerChoice === "scissors"){
+    resultParagraph.textContent = "You win"
+    winsSpan.textContent= +winsSpan.textContent +1
+  }
+}
+const fightWithPaper = computerChoice =>{
+  if (computerChoice === "rock"){
+    resultParagraph.textContent = "You win"
+    winsSpan.textContent= +winsSpan.textContent +1
+  }
+  else if (computerChoice === "paper"){
+    resultParagraph.textContent = "It's a draw"
+    drawsSpan.textContent= +drawsSpan.textContent +1
+  }
+  else if (computerChoice === "scissors"){
+    resultParagraph.textContent = "You lose"
+    losesSpan.textContent= +losesSpan.textContent +1
+  }
+}
+const fightWithScissors = computerChoice =>{
+  if (computerChoice === "rock"){
+    resultParagraph.textContent = "You lose"
+    losesSpan.textContent= +losesSpan.textContent +1
+  }
+  else if (computerChoice === "paper"){
+    resultParagraph.textContent = "You win"
+    winsSpan.textContent= +winsSpan.textContent +1
+  }
+  else if (computerChoice === "scissors"){
+    resultParagraph.textContent = "It's a draw"
+    drawsSpan.textContent= +drawsSpan.textContent +1
+  }
+}
+
+const showResult= (userChoice, computerChoice) =>{
+  if (userChoice === "rock"){
+    fightWithRock(computerChoice)
+  }
+  else if (userChoice === "paper"){
+    fightWithPaper(computerChoice)
+  }
+  else if (userChoice === "scissors"){
+    fightWithScissors(computerChoice)
+  }
+}
 
 const draw = () =>{
   const choices = ["rock", "paper", "scissors"]
   const index = Math.floor(Math.random()*3)
-  const randomChoice = choices[index]
-  console.log(randomChoice)
+  const computerChoice = choices[index]
+  computerChoiceImage.style.display = "block"
+  computerChoiceImage.classList.add(computerChoice)
+  return computerChoice
 }
 
 const removeHover = () =>{
@@ -17,10 +79,11 @@ const removeHover = () =>{
 
 const getChoice = e =>{
   const element = e.target
-  console.log(element.getAttribute('alt'))
+  const userChoice= element.getAttribute('alt')
   element.classList.add("clicked")
   removeHover()
-  draw()
+  const computerChoice= draw()
+  showResult(userChoice,computerChoice)
 }
 
 for (const image of images){
